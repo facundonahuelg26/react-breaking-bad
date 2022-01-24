4d762e09736647f0e4c6c1a728bdc37a4d35e99a
Facundo <facundonahuelg26@gmail.com>
2022-01-24 12:14:07 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+  const [quote, setQuote] = useState([]);
+  //const [loading, setLoading] = useState(false);
+
+  const updateQuote = async () => {
+    //setLoading(true)
+    const numberRandom = Math.ceil(Math.random() * 10)
+    const urlApi = `https://www.breakingbadapi.com/api/characters/${numberRandom}`
+    const res = await fetch(urlApi);
+    //console.log(res);
+    const data = await res.json();
+    setQuote(data)
+    // console.log(data);
+    //   data.map((el) => {
+    //     console.log(el.key);
+    //   })
+       
+  }
+
+  useEffect(() => {
+     updateQuote();
+  }, []);
+  
+
+  const handleClick = () => {
+    updateQuote()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        quote.map(el => (
+          <div key={el.char_id}>
+            <img src={el.img} alt={el.name} />
+            <h2>{el.name}</h2>
+            <button onClick={handleClick}>Change</button>
+          </div>
+        ))
+      }
+      
+      
     </div>
+    
   );
 }
 
